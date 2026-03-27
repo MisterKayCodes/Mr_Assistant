2dd3eda4a9aba7dd270dc7d1c8054038e76a8f2a
MisterKayCodes <kaycris10@gmail.com>
2026-03-27 14:05:22 +0100

    First Commit

--- Mister_Starter_Pack/setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Setting up virtual environment...
-python -m venv venv
-echo Activating virtual environment and installing requirements...
-call venv\Scripts\activate
-pip install -r requirements.txt
-echo Setup complete!
-pause
